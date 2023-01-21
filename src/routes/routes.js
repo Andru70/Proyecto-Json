b158a6c6b634438f703280bb608d5af4c0baa130
Santiago Ruiz <sanandres2504@gmail.com>
2023-01-21 14:32:16 -0500

    Editar producto solucionado

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,17 +3,18 @@ const fs = require("fs");
 const path = require('path')
 const express = require("express");
 
-// const pathJson = path.join(__dirname, '../../clientes.json')
-// const file_clientes = fs.readFileSync(pathJson, 'utf-8');
-// let clientes  = JSON.parse(file_clientes);
 
 const pathJson1 = path.join(__dirname, '../../usuarios.json')
 const file_usuarios = fs.readFileSync(pathJson1, 'utf-8');
-let usuarios  = JSON.parse(file_usuarios);
+let usuarios = JSON.parse(file_usuarios);
 
 const pathJson2 = path.join(__dirname, '../db_json/productos.json')
 const file_productos = fs.readFileSync(pathJson2, 'utf-8');
-let listproductos  = JSON.parse(file_productos);
+let listproductos = JSON.parse(file_productos);
+
+// const pathtest = path.join(__dirname, '../../productos.json')
+// const file_test = fs.readFileSync(pathtest, 'utf-8');
+// let listtest = JSON.parse(file_test);
 
 module.exports = (app) => {
   app.use(express.json());
@@ -62,48 +63,55 @@ module.exports = (app) => {
     res.render("register");
   });
 
-  //Esto está bien
   app.get("/json", (req, res) => {
     res.sendFile('C:\\xampp\\htdocs\\manejo_json\\src\\db_json\\productos.json')
   });
+
+  // app.get("/jsontest", (req, res) => {
+  //   res.sendFile('C:\\xampp\\htdocs\\manejo_json\\productos.json')
+  // });
 
   app.get("/new_product", (req, res) => {
     res.render("new_product");
   });
 
+  app.get("/edit_product", (req, res) => {
+    res.render("edit_product");
+  });
+
   app.post("/login", (req, res) => {
+
     let jsonData = require("../../usuarios.json");
-    //let jsonData2 = require("../../clientes.json");
-    var json = [];
+
     var user = req.body.email;
     var pass = req.body.password;
     var userbuscado
     var rol;
 
     userbuscado = jsonData.find(dato => dato.email === user)
-    
+
     rol = userbuscado.rol
-    console.log(rol)
+    // console.log(rol)
 
-      if (userbuscado && userbuscado.password == pass && rol == "User") {
+    if (userbuscado && userbuscado.password == pass && rol == "User") {
 
-        req.session.loggedin = true;
-        req.session.nombre = userbuscado.nombre
-        nombre_user = req.session.nombre
+      req.session.loggedin = true;
+      req.session.nombre = userbuscado.nombre
+      nombre_user = req.session.nombre
 
-        res.redirect("/");
+      res.redirect("/");
 
-      } else if(userbuscado && userbuscado.password == pass && rol == "Admin") {
-        req.session.loggedin = true;
-        req.session.nombre = userbuscado.nombre
-        nombre_user = req.session.nombre
-        res.redirect("/admin");
-      }else{
-        res.redirect("/login");
-      }
+    } else if (userbuscado && userbuscado.password == pass && rol == "Admin") {
+      req.session.loggedin = true;
+      req.session.nombre = userbuscado.nombre
+      nombre_user = req.session.nombre
+      res.redirect("/admin");
+    } else {
+      res.redirect("/login");
+    }
   });
 
- 
+
   app.post("/register", (req, res) => {
     let nombre = req.body.nombre;
     let email = req.body.email;
@@ -111,26 +119,26 @@ module.exports = (app) => {
     let rol = "User";
 
     var nuevo_usuario = {
-        nombre,
-        email,
-        password,
-        rol
+      nombre,
+      email,
+      password,
+      rol
     }
 
     usuarios.push(nuevo_usuario)
 
-    fs.writeFileSync("usuarios.json",JSON.stringify(usuarios),
+    fs.writeFileSync("usuarios.json", JSON.stringify(usuarios),
       "utf8", (err) => {
         if (err) throw err;
         console.log("The file has been saved!");
       }
     );
-  
+
     res.redirect("/");
   });
 
   app.post("/new_product", (req, res) => {
-    
+
     let num_pro = listproductos.productos.length;
     let id = num_pro + 1;
     let id_cat = id;
@@ -141,40 +149,57 @@ module.exports = (app) => {
     let imagen = req.body.url_imagen;
 
     var nuevo_producto = {
-        id,
-        id_cat,
-        nombre,
-        precio,
-        descripcion,
-        imagen
+      id,
+      id_cat,
+      nombre,
+      precio,
+      descripcion,
+      imagen
     }
 
     console.log(__dirname)
 
     fetch('http://localhost:3031/json')
-    .then(response => response.json())
-    .then(data => {
-      data.productos.push(nuevo_producto);
+      .then(response => response.json())
+      .then(data => {
+        data.productos.push(nuevo_producto);
 
-      fs.writeFile(pathJson2, JSON.stringify(data), (err) => {
-        if (err) {
+        fs.writeFile(pathJson2, JSON.stringify(data), (err) => {
+          if (err) {
             console.error(err);
-        }
-    });
-    })
-    .catch(error => {
+          }
+        });
+      })
+      .catch(error => {
         console.error(error);
-    });
+      });
 
     res.redirect("/admin")
-    
+
 
   });
 
-  app.get('/compra', (req, res)=>{
-      console.log(nombre_user)
-      let nombre = document.getElementById("nombre")
-      res.redirect('/')
+  app.post("/act_jsonp", (req, res) => {
+
+    let data = {
+      "productos": req.body
+    }
+
+    //console.log(JSON.stringify(data))
+
+    fs.writeFile(pathJson2, JSON.stringify(data), (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+
+    res.redirect("/admin")
+  })
+
+  app.get('/compra', (req, res) => {
+    console.log(nombre_user)
+    let nombre = document.getElementById("nombre")
+    res.redirect('/')
   })
 
   app.get("/logout", function (req, res) {
